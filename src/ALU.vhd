9b397d86299d8fd2087b938fdad967fdf9378259
Zekepk45 <zekepk456@gmail.com>
2025-05-05 21:30:09 -0600

    dsdfs

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -1,34 +1,51 @@
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/18/2025 02:50:18 PM
--- Design Name: 
--- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
 ----------------------------------------------------------------------------------
 
+-- Company: 
 
+-- Engineer: 
+
+-- 
+
+-- Create Date: 04/18/2025 02:50:18 PM
+
+-- Design Name: 
+
+-- Module Name: ALU - Behavioral
+
+-- Project Name: 
+
+-- Target Devices: 
+
+-- Tool Versions: 
+
+-- Description: 
+
+-- 
+
+-- Dependencies: 
+
+-- 
+
+-- Revision:
+
+-- Revision 0.01 - File Created
+
+-- Additional Comments:
+
+-- 
+
+----------------------------------------------------------------------------------
+ 
+ 
 library IEEE;
+
 use IEEE.STD_LOGIC_1164.ALL;
-
+ 
 -- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- arithmetic functions with Signed or Unsigned values
+
+--use IEEE.NUMERIC_STD.ALL;
  
 -- Uncomment the following library declaration if instantiating
 
@@ -39,68 +56,134 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
  
 entity ALU is
+
     Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
+
            i_B : in STD_LOGIC_VECTOR (7 downto 0);
+
            i_op : in STD_LOGIC_VECTOR (2 downto 0);
+
            o_result : out STD_LOGIC_VECTOR (7 downto 0);
+
            o_flags : out STD_LOGIC_VECTOR (3 downto 0));
+
 end ALU;
  
 architecture Behavioral of ALU is
+ 
     component ripple_adder is
+
         port(
-            A: in std_logic_vector(3 downto 0);
-            B: in std_logic_vector(3 downto 0);
-            Cin : in STD_LOGIC; 
-            S : out std_logic_vector(3 downto 0);
-            Cout : out std_logic 
-          );
-          end component ripple_adder;
-        signal w_sum: std_logic_vector(7 downto 0);
-        signal w_lower_carry: STD_LOGIC;
-        signal w_upper_carry: STD_LOGIC;
-        signal resultOUT : std_logic_vector(7 downto 0);
-        signal w_B_ALU : std_logic_vector(7 downto 0);
+
+           A : in STD_LOGIC_VECTOR (3 downto 0);
+
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+
+           Cin : in STD_LOGIC;
+
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+
+           Cout : out STD_LOGIC
+
+        );
+
+    end component ripple_adder;
+
+    --All signals needed for summation (and subtraction), or, and, carry, and results
+
+    signal w_sum, w_or, w_and, w_newB, w_result : std_logic_vector(7 downto 0);
+
+    signal w_carry, w_carry2  : std_logic;
  
 begin
-    w_B_ALU <= i_B when (i_OP(0) = '0') else
-                        not i_B;
-    Rippler_Lower : ripple_adder
-        port map(
-            A => i_A(3 downto 0),
-            B => w_B_ALU(3 downto 0),
-            Cin => i_OP(0),
-            S => w_sum(3 downto 0),
-            Cout => w_lower_carry
-          );
-     Ripple_Upper : ripple_adder 
-        port map(
-            A => i_A(7 downto 4),
-            B => w_B_ALU(7 downto 4),
-            Cin => w_lower_carry,
-            S => w_sum(7 downto 4),
-            Cout => w_upper_carry
-          );
-    -- MUX Stuff Now
-    resultOUT <= w_sum when i_OP = "000" else
-                 w_sum when i_OP = "001" else
-                 i_A and i_B when i_OP = "010" else
-                 i_A and i_B when i_OP = "011" else
-                 w_sum;
-    o_result <= resultOUT;
-    o_flags(0) <= not (i_OP(0) xor i_A(7) xor i_B(7)) and (i_A(7) xor w_sum(7)) and (not i_OP(1));
-    o_flags(1) <= w_upper_carry and (not i_OP(1));
-    o_flags(3) <= resultOUT(7);
-    o_flags(2) <= '1' when (resultOUT = x"00") else
-                '0';
-end Behavioral;
  
+    --start with the easy "and/or" operations
+
+    w_or <= i_A or i_B;
+
+    w_and <= i_A and i_B;
+
+    --Makes it so we have a signed version if i_B. For subtraction
+
+    w_newB <= (not i_B) when (i_op(0) = '1') else i_B;
+ 
+    rippleadder1 : ripple_adder
+
+    port map(
+
+        A => i_A(3 downto 0),
+
+        --Use w_newB here since it now has a sign based on the input
+
+        B => w_newB(3 downto 0),
+
+        --start with the sign
+
+        Cin => i_op(0),
+
+        S => w_sum(3 downto 0),
+
+        Cout => w_carry
+
+    );
+
+    rippleadder2 : ripple_adder
+
+    port map(
+
+        A => i_A(7 downto 4),
+
+        --Same as noted above for why we use w_newB
+
+        B => w_newB(7 downto 4),
+
+        --takes the Cout from the previous rippleadder
+
+        Cin => w_carry,
+
+        S => w_sum(7 downto 4),
+
+        Cout => w_carry2
+
+    );
+ 
+    with i_op select
+
+    w_result <= w_sum when "000",--aDd
+
+                w_sum when "001", --sub
+
+                w_and when "010", --aNd
+
+                w_or when "011", --or
+
+                "00000000" when others; --catch all
+
+    o_result <= w_result;
+
+    --The "NZCV"
+
+    --N
+
+    o_flags(3) <= w_result(7);
+
+    --Z
+
+    o_flags(2) <= not (w_result(0) or w_result(1) or w_result(2) or w_result(3) or w_result(4) or w_result(5) or w_result(6) or w_result(7));
+
+    --C
+
+    o_flags(1) <= w_carry2 and (not i_op(1));
+
+    --V
+
+    o_flags(0) <= (not i_op(1)) and (w_sum(7) xor i_A(7)) and (not (i_op(0) xor i_A(7) xor i_B(7)));
+ 
+    
 
 
+ 
+end Behavioral;
 
-
-
-
-
-
+ 
 
