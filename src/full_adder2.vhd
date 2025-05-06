5fe00d4514c072ddba0a8b44793f41a36924561b
Zekepk45 <zekepk456@gmail.com>
2025-05-05 21:09:56 -0600

    newasfl

---
+++ src/full_adder2.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity full_adder2 is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Cin : in  STD_LOGIC;
+           S : out  STD_LOGIC;
+           Cout : out  STD_LOGIC);
+end full_adder2;
+
+architecture Behavioral of full_adder2 is
+begin
+
+        S <= A XOR B XOR Cin;
+        Cout <= (A AND B) OR (B AND Cin) OR (A AND Cin);
+        
+end Behavioral;
