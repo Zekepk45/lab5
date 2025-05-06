e63d0d3540a9f9bf78453656e162b8bb358d648e
Zekepk45 <zekepk456@gmail.com>
2025-05-05 20:54:01 -0600

    newasf

--- src/ripple_adder.vhd
+++ src/ripple_adder.vhd
@@ -2,10 +2,10 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity ripple_adder is
-    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
-           B : in STD_LOGIC_VECTOR (3 downto 0);
+    Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
+           B : in STD_LOGIC_VECTOR (7 downto 0);
            Cin : in STD_LOGIC;
-           S : out STD_LOGIC_VECTOR (3 downto 0);
+           S : out STD_LOGIC_VECTOR (7 downto 0);
            Cout : out STD_LOGIC);
 end ripple_adder;
 
